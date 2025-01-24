871aba0b098b7eb13a249e266f785defc807c756
tuanemuy <yumenaut@gmail.com>
2025-01-24 19:20:00 +0900

    debug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-export default defineConfig(
-  process.env.DATABASE_AUTH_TOKEN
-    ? {
-        out: "./lib/db/drizzle",
-        schema: "./lib/db/schema.ts",
-        dialect: "turso",
-        dbCredentials: {
-          url: process.env.DATABASE_URL,
-          authToken: process.env.DATABASE_AUTH_TOKEN,
-        },
-      }
-    : {
-        out: "./lib/db/drizzle",
-        schema: "./lib/db/schema.ts",
-        dialect: "sqlite",
-        dbCredentials: {
-          url: process.env.DATABASE_URL,
-        },
-      },
-);
+export default defineConfig({
+  out: "./lib/db/drizzle",
+  schema: "./lib/db/schema.ts",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  },
+});
