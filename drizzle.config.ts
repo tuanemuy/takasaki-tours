a1638087a8972be5fbe9f4eedb2d9655d14db444
tuanemuy <yumenaut@gmail.com>
2025-01-24 19:14:03 +0900

    debug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,15 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-const tursoAuthToken = process.env.DATABASE_AUTH_TOKEN || undefined;
-
 export default defineConfig(
-  tursoAuthToken
+  process.env.DATABASE_AUTH_TOKEN
     ? {
         out: "./lib/db/drizzle",
         schema: "./lib/db/schema.ts",
         dialect: "turso",
         dbCredentials: {
           url: process.env.DATABASE_URL,
-          authToken: tursoAuthToken,
+          authToken: process.env.DATABASE_AUTH_TOKEN,
         },
       }
     : {
