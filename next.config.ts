3141ac7104fd2f32f0d5c30022b175497965ba25
tuanemuy <yumenaut@gmail.com>
2025-01-25 00:07:20 +0900

    debug

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingIncludes: {
-    "/": ["./node_modules/@libsql/**/*"],
+    "/": [
+      "./node_modules/@libsql/**/*",
+      "./node_modules/drizzle-orm/sqlite-core/**/*",
+    ],
   },
   experimental: {
     serverActions: {
