14ab24284640a32f0d0637d72f016bc897f8d908
tuanemuy <yumenaut@gmail.com>
2025-01-24 23:18:47 +0900

    debug

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingIncludes: {
-    "/": ["node_modules/@libsql/**/*"],
+    "/": ["./node_modules/@libsql/**/*"],
   },
   experimental: {
     serverActions: {
